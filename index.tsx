66c1285e96f7e6d7b7132f6272154e653dbc7bfe
shuflovic <pavel.skuta@gmail.com>
2025-10-04 13:51:01 +1300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -7,6 +6,7 @@ const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
+console.log('Mounting React app to root element:', rootElement);
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
