ab8c7afea2736c5e4afa91dc0bba43cb68794044
shuflovic <pavel.skuta@gmail.com>
2025-10-04 18:22:23 +1300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/town_66/',
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
   plugins: [react()],
-  base: '/town_66' // IMPORTANT: Set this to your repository name
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
+});
