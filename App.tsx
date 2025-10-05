17c67a561fb2a51ae81a9a18d40e44d5630ffbfc
shuflovic <pavel.skuta@gmail.com>
2025-10-05 17:45:53 +1300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -239,8 +239,7 @@ const App: React.FC = () => {
             const file = new File([blob], `town${gridSize}${gridSize}-board.png`, { type: 'image/png' });
             const shareData = {
                 title: `Town ${gridSize}${gridSize} Score`,
-                text: `I scored ${lastScores[gridSize]} in Town ${gridSize}${gridSize}! Can you beat my score? #Town${gridSize}${gridSize}`,
-                url: 'https://shuflovic.github.io/town_66',
+                text: `I scored ${lastScores[gridSize]} in Town ${gridSize}${gridSize}! Can you beat my score?\n #Town${gridSize}${gridSize} https://shuflovic.github.io/town_66',
                 files: [file],
             };
             await navigator.share(shareData);
